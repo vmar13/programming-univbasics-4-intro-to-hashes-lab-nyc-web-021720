4238dec36eaff3ed3e6291881f98768f9476a3dd
Vanessa Martinez <vmar76@gmail.com>
2020-02-12 03:14:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,28 +1,37 @@
 def new_hash
-  # return an empty hash
+  hash=Hash.new 
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  new_hash = {
+    name: "Sam", age: 31
+  }
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  hash = {
+  :name => 'Grace Hopper'
+  }
 end
 
 def id_generator
-  # return a hash with a key :id assigned to positive integer
+  hash = {
+    :id => 5
+  }
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+  hash = {
+    :name => 'Grace Hopper'
+  }
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+  hash[key]
 end
 
 def update_counting_hash(hash, key)
+  hash[:key] = "hello"=>1 
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
